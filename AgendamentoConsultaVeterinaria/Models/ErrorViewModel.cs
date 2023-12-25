0847535cb54c1fc0ec09e6445c58e279cfb15466
Lucas Cardoso <lucasreis-@outlook.com>
2023-12-25 14:37:17 -0300

    iniciando o projeto

---
+++ AgendamentoConsultaVeterinaria/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AgendamentoConsultaVeterinaria.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
